05ffeb75ec1c75ecb3672adae1a7012b6c3036db
Auskennfuchs <auskennfuchs@auskennfuchs.de>
2017-06-11 23:28:47 +0200

    initial commit

---
+++ PlaylistGenerator/MP3ID3Tag.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace PlaylistGenerator
+{
+    class MusicID3Tag
+    {
+
+        public byte[] TAGID = new byte[3];      //  3
+        public byte[] Title = new byte[30];     //  30
+        public byte[] Artist = new byte[30];    //  30 
+        public byte[] Album = new byte[30];     //  30 
+        public byte[] Year = new byte[4];       //  4 
+        public byte[] Comment = new byte[30];   //  30 
+        public byte[] Genre = new byte[1];      //  1
+
+    }
+
+    class MP3ID3Tag
+    {
+
+        private string title, artist, album, year, comment, genre;
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+        }
+
+        public string Artist
+        {
+            get
+            {
+                return artist;
+            }
+        }
+
+        public bool ReadTag(string file)
+        {
+            using (FileStream fs = File.OpenRead(file))
+            {
+                if (fs.Length >= 128)
+                {
+                    MusicID3Tag tag = new MusicID3Tag();
+                    fs.Seek(-128, SeekOrigin.End);
+                    fs.Read(tag.TAGID, 0, tag.TAGID.Length);
+                    fs.Read(tag.Title, 0, tag.Title.Length);
+                    fs.Read(tag.Artist, 0, tag.Artist.Length);
+                    fs.Read(tag.Album, 0, tag.Album.Length);
+                    fs.Read(tag.Year, 0, tag.Year.Length);
+                    fs.Read(tag.Comment, 0, tag.Comment.Length);
+                    fs.Read(tag.Genre, 0, tag.Genre.Length);
+                    string theTAGID = Encoding.Default.GetString(tag.TAGID);
+
+                    if (theTAGID.Equals("TAG"))
+                    {
+                        title = Encoding.Default.GetString(tag.Title);
+                        artist = Encoding.Default.GetString(tag.Artist);
+                        album = Encoding.Default.GetString(tag.Album);
+                        year = Encoding.Default.GetString(tag.Year);
+                        comment = Encoding.Default.GetString(tag.Comment);
+                        genre = Encoding.Default.GetString(tag.Genre);
+                    }
+                }
+            }
+            return true; 
+        }
+    }
+}
---
+++ PlaylistGenerator/PlaylistGenerator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlaylistGenerator.Properties;
+using System.IO;
+using System.Windows.Forms;
+using mp3info;
+
+namespace PlaylistGenerator
+{
+    class PlaylistGenerator
+    {
+        public static bool Generate(string rootpath, List<Playlist> playlists, bool overwrite)
+        {
+            string plname = "";
+            foreach (Playlist pl in playlists)
+            {
+                try
+                {
+                    if (pl.files.Count > 0)
+                    {
+                        plname = rootpath + "\\" + pl.playlistname + ".m3u";
+                        int i = 1;
+                        while (File.Exists(plname) && !overwrite)
+                        {
+                            plname = rootpath + "\\" + pl.playlistname + "_" + i.ToString("D3") + ".m3u";
+                            i++;
+                        }
+
+                        using (StreamWriter f = new StreamWriter(plname, false, System.Text.Encoding.Default))
+                        {
+                            WriteM3UHeader(f);
+                            foreach (string entry in pl.files)
+                            {
+                                WriteExtInf(f, entry, rootpath, pl.playlistname);
+                                WriteFileLine(f, entry, rootpath, pl.playlistname);
+                            }
+                        }
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(plname+"\n"+ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static void WriteM3UHeader(StreamWriter fs)
+        {
+            fs.WriteLine("#EXTM3U");
+        }
+
+        private static void WriteExtInf(StreamWriter fs, string file, string rootpath, string plname)
+        {
+            string name = createFileTitle(file, rootpath, plname);
+
+            string line = "#EXTINF:-1," + name;
+
+            fs.WriteLine(line);
+        }
+
+        private static void WriteFileLine(StreamWriter fs, string file, string rootpath, string plname)
+        {
+            string name = file;
+            name = name.Replace(rootpath + "\\", "");
+            fs.WriteLine(name);
+        }
+
+        private static string createFileTitle(string file, string rootpath, string plname)
+        {
+            string title=file.Substring(0,file.Length-4);
+            title = title.Replace(rootpath + "\\" + plname + "\\", "");
+            title = title.Replace(rootpath + "\\", "");
+
+            mp3info.mp3info info = new mp3info.mp3info(file);
+            info.ReadAll(file);
+            if (info.hasID3v2) {
+                if (info.id3v2.Title.Length > 0) {
+                    title = info.id3v2.Title;
+                }
+                if (info.id3v2.Artist.Length > 0) {
+                    title += " - " + info.id3v2.Artist;
+                }
+                title = title.Replace("\0", "");
+                title = title.Replace("ÿþ", "");
+            } else if (info.hasID3v1) {
+                if (info.id3v1.Title.Length > 0) {
+                    title = info.id3v1.Title;
+                }
+                if (info.id3v1.Artist.Length > 0) {
+                    title += " - " + info.id3v1.Artist;
+                }
+            }
+            return title;
+        }
+        
+    }
+}
